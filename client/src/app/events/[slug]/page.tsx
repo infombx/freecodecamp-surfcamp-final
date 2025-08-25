1f36af0b1d2f4130e82a59a650ee366c273d3411
infombx <info@metabox.mu>
2025-08-25 15:22:57 +0400

    Update page.tsx

--- client/src/app/events/[slug]/page.tsx
+++ client/src/app/events/[slug]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import type { EventProps } from "@/types";
 import { ContentList } from "@/components/ContentList";
 import { Card, type CardProps } from "@/components/Card";
